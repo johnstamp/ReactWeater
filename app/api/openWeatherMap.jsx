62feb421d1013bc9380ec0f7bb372c1470d91f2c
John <john.stamp@directski.com>
2016-06-10 16:42:36 +0100

    added foundation and jquery

--- app/api/openWeatherMap.jsx
+++ app/api/openWeatherMap.jsx
@@ -1,6 +1,6 @@
 var axios = require('axios');
 
-const OPEN_WEATHER_MAP_URL='http://api.openweathermap.org/data/2.5/weather?appid=54024a644a90ccf1eea84000e56fd2a5&units=imperial';
+const OPEN_WEATHER_MAP_URL='http://api.openweathermap.org/data/2.5/weather?appid=54024a644a90ccf1eea84000e56fd2a5&units=metric';
 
 //54024a644a90ccf1eea84000e56fd2a5
 
@@ -8,9 +8,8 @@ module.exports={
   getTemp: function (location){
     var encodedLocation = encodeURIComponent(location);
 
-    var requestUrl = `${OPEN_WEATHER_MAP_URL}+&q=${encodedLocation}`
-    return axios.get(requestUrl).then(function(res){
-
+    var requestUrl = `${OPEN_WEATHER_MAP_URL}+&q=${encodedLocation}`;
+    axios.get(requestUrl).then(function(res){
         if(res.data.code&&res.data.message){
           throw new Error(res.data.message);
         }else{
